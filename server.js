4b325c5c122997947382331fa4494772b083a876
Aymen BEN MILED <abenmiled@ecovadis.com>
2019-11-18 15:52:57 +0100

    set config in multiple files.

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const app = express();
 const port = 8000;
 
 // module variables
-const config = require('./configuration/config.json');
-const defaultConfig = config.development;
+const defaultConfig = require('./configuration/config.json');
+//const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || 'development';
-const environmentConfig = config[environment];
+// const environmentConfig = config[environment];
+const environmentConfig = require(`./configuration/config.${environment}.json`);
 const finalConfig = _.merge(defaultConfig, environmentConfig);
 global.gConfig = finalConfig;
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
     console.log(`global.gConfig: ${process.env.NODE_ENV}`);
-    console.log(`global.gConfig: ${JSON.stringify(global.gConfig, undefined, global.gConfig.json_indentation)}`);
+    console.log(`final Config: ${JSON.stringify(finalConfig)}`);
+    // console.log(`global.gConfig: ${JSON.stringify(config)}`);
 });
